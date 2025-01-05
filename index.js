c834b9554c16dd926bbf2726eec66d609ce27865
Ben <benbatres1212@gmail.com>
2025-01-05 11:21:14 -0800

    testing touchstart

--- index.js
+++ index.js
@@ -70,123 +70,123 @@ sr.reveal('.section__title', {delay:300, origin:'bottom'});
 sr.reveal('.project', {delay:200, origin:'left'});
 sr.reveal('.footer__row', {delay:200, origin:'top'});
 
-function displayTreactCloneProjectDescription() {
-    const displayProject = document.querySelector("#treact__project--description")
+// function displayTreactCloneProjectDescription() {
+//     const displayProject = document.querySelector("#treact__project--description")
     
-    if (displayProject.innerHTML.trim() === "") {
-        displayProject.innerHTML = `
-            <div>
-                <h3 class="project__description--title onTouch__description--title">
-                    Treact Clone Project
-                </h3>
-                <h4 class="project__description--sub-title onTouch__description--sub-title">
-                    Html, CSS, JavaScript
-                </h4>
-                <p class="project__description--para onTouch__description--para">
-                    I recreated this project with the skills I learned from Frontend Simplified, and I made it responsive on all devices.
-                </p>
-                <div class="project__description--links onTouch__description--links">
-                    <a href="https://github.com/Chaparro-ben/projects/blob/master/index.html" class="project__description--link onTouch__description--link" target="_blank">
-                        <i class="fab fa-github"></i>
-                    </a>
-                    <a href="https://chaparro-ben.github.io/projects/" target="_blank" class="project__description--link onTouch__description--link">
-                        <i class="fas fa-link"></i>
-                    </a>
-                </div>
-            </div>` 
-    } else {
-        displayProject.innerHTML = ""
-    }
-}
-document.querySelector(".treact__project").addEventListener("touchstart", displayTreactCloneProjectDescription)
+//     if (displayProject.innerHTML.trim() === "") {
+//         displayProject.innerHTML = `
+//             <div>
+//                 <h3 class="project__description--title onTouch__description--title">
+//                     Treact Clone Project
+//                 </h3>
+//                 <h4 class="project__description--sub-title onTouch__description--sub-title">
+//                     Html, CSS, JavaScript
+//                 </h4>
+//                 <p class="project__description--para onTouch__description--para">
+//                     I recreated this project with the skills I learned from Frontend Simplified, and I made it responsive on all devices.
+//                 </p>
+//                 <div class="project__description--links onTouch__description--links">
+//                     <a href="https://github.com/Chaparro-ben/projects/blob/master/index.html" class="project__description--link onTouch__description--link" target="_blank">
+//                         <i class="fab fa-github"></i>
+//                     </a>
+//                     <a href="https://chaparro-ben.github.io/projects/" target="_blank" class="project__description--link onTouch__description--link">
+//                         <i class="fas fa-link"></i>
+//                     </a>
+//                 </div>
+//             </div>` 
+//     } else {
+//         displayProject.innerHTML = ""
+//     }
+// }
+// document.querySelector(".treact__project").addEventListener("touchstart", displayTreactCloneProjectDescription)
 
-function displayMovieAPIProjectDescription() {
-    const displayProject = document.querySelector("#movie__API--description")
+// function displayMovieAPIProjectDescription() {
+//     const displayProject = document.querySelector("#movie__API--description")
     
-    if (displayProject.innerHTML.trim() === "") {
-        displayProject.innerHTML = 
-        `
-            <div>
-                <h3 class="project__description--title onTouch__description--title">
-                    Movie API
-                </h3>
-                <h4 class="project__description--sub-title onTouch__description--sub-title">
-                    Html, CSS, JavaScript, and React
-                </h4>
-                <p class="project__description--para onTouch__description--para">
-                    This website I created uses API. This website shows any movie the users searches up and it has a sort selection that sorts from latest to oldest and oldest to latest.
-                </p>
-                <div class="project__description--links onTouch__description--links">
-                    <a href="https://github.com/Chaparro-ben/movie-api" class="project__description--link onTouch__description--link" target="_blank">
-                        <i class="fab fa-github"></i>
-                    </a>
-                    <a href="https://movie-api-taupe-five.vercel.app/" target="_blank" class="project__description--link onTouch__description--link">
-                        <i class="fas fa-link"></i>
-                    </a>
-                </div>
-            </div>` 
-    } else {
-        displayProject.innerHTML = ""
-    }
-}
-document.querySelector(".movie__API").addEventListener("touchstart", displayMovieAPIProjectDescription)
+//     if (displayProject.innerHTML.trim() === "") {
+//         displayProject.innerHTML = 
+//         `
+//             <div>
+//                 <h3 class="project__description--title onTouch__description--title">
+//                     Movie API
+//                 </h3>
+//                 <h4 class="project__description--sub-title onTouch__description--sub-title">
+//                     Html, CSS, JavaScript, and React
+//                 </h4>
+//                 <p class="project__description--para onTouch__description--para">
+//                     This website I created uses API. This website shows any movie the users searches up and it has a sort selection that sorts from latest to oldest and oldest to latest.
+//                 </p>
+//                 <div class="project__description--links onTouch__description--links">
+//                     <a href="https://github.com/Chaparro-ben/movie-api" class="project__description--link onTouch__description--link" target="_blank">
+//                         <i class="fab fa-github"></i>
+//                     </a>
+//                     <a href="https://movie-api-taupe-five.vercel.app/" target="_blank" class="project__description--link onTouch__description--link">
+//                         <i class="fas fa-link"></i>
+//                     </a>
+//                 </div>
+//             </div>` 
+//     } else {
+//         displayProject.innerHTML = ""
+//     }
+// }
+// document.querySelector(".movie__API").addEventListener("touchstart", displayMovieAPIProjectDescription)
 
-function displayLibraryProjectDescription() {
-    const displayProject = document.querySelector("#library__project--description")
+// function displayLibraryProjectDescription() {
+//     const displayProject = document.querySelector("#library__project--description")
     
-    if (displayProject.innerHTML.trim() === "") {
-        displayProject.innerHTML = `
-            <div>
-                <h3 class="project__description--title onTouch__description--title">
-                    Library React Project
-                </h3>
-                <h4 class="project__description--sub-title onTouch__description--sub-title">
-                    Html, CSS, JavaScript, and React
-                </h4>
-                <p class="project__description--para onTouch__description--para">
-                    In this project I learned how to use componenets, hooks, and props to make a responsive and working website.
-                </p>
-                <div class="project__description--links onTouch__description--links">
-                    <a href="https://github.com/Chaparro-ben/library-react" class="project__description--link onTouch__description--link" target="_blank">
-                        <i class="fab fa-github"></i>
-                    </a>
-                    <a href="https://library-react-flor4pl34-chaparro-bens-projects.vercel.app/" target="_blank" class="project__description--link onTouch__description--link">
-                        <i class="fas fa-link"></i>
-                    </a>
-                </div>
-            </div>` 
-    } else {
-        displayProject.innerHTML = ""
-    }
-}
-document.querySelector(".library__project").addEventListener("touchstart", displayLibraryProjectDescription)
+//     if (displayProject.innerHTML.trim() === "") {
+//         displayProject.innerHTML = `
+//             <div>
+//                 <h3 class="project__description--title onTouch__description--title">
+//                     Library React Project
+//                 </h3>
+//                 <h4 class="project__description--sub-title onTouch__description--sub-title">
+//                     Html, CSS, JavaScript, and React
+//                 </h4>
+//                 <p class="project__description--para onTouch__description--para">
+//                     In this project I learned how to use componenets, hooks, and props to make a responsive and working website.
+//                 </p>
+//                 <div class="project__description--links onTouch__description--links">
+//                     <a href="https://github.com/Chaparro-ben/library-react" class="project__description--link onTouch__description--link" target="_blank">
+//                         <i class="fab fa-github"></i>
+//                     </a>
+//                     <a href="https://library-react-flor4pl34-chaparro-bens-projects.vercel.app/" target="_blank" class="project__description--link onTouch__description--link">
+//                         <i class="fas fa-link"></i>
+//                     </a>
+//                 </div>
+//             </div>` 
+//     } else {
+//         displayProject.innerHTML = ""
+//     }
+// }
+// document.querySelector(".library__project").addEventListener("touchstart", displayLibraryProjectDescription)
 
-function displayNetflixProjectDescription() {
-    const displayProject = document.querySelector("#netflix__project--description")
+// function displayNetflixProjectDescription() {
+//     const displayProject = document.querySelector("#netflix__project--description")
     
-    if (displayProject.innerHTML.trim() === "") {
-        displayProject.innerHTML = `
-            <div>
-                <h3 class="project__description--title onTouch__description--title">
-                    Netflix Clone
-                </h3>
-                <h4 class="project__description--sub-title onTouch__description--sub-title">
-                    TypeScript, JavaScript, and Firebase
-                </h4>
-                <p class="project__description--para onTouch__description--para">
-                    I created a Netflix clone using React and Firebase to practice building a dynamic and responsive web application. The app fetches data from the TMDb API to display movies and TV shows by category, supports user authentication, and is fully responsive. Through this project, I strengthened my skills in API intergation, React hooks, and UI design.
-                </p>
-                <div class="project__description--links onTouch__description--links">
-                    <a href="https://github.com/Chaparro-ben/netflix-clone" class="project__description--link onTouch__description--link" target="_blank">
-                        <i class="fab fa-github"></i>
-                    </a>
-                    <a href="https://netflix-clone-murex-gamma.vercel.app/" target="_blank" class="project__description--link onTouch__description--link">
-                        <i class="fas fa-link"></i>
-                    </a>
-                </div>
-            </div>` 
-    } else {
-        displayProject.innerHTML = ""
-    }
-}
-document.querySelector(".netflix__project").addEventListener("touchstart", displayNetflixProjectDescription)
+//     if (displayProject.innerHTML.trim() === "") {
+//         displayProject.innerHTML = `
+//             <div>
+//                 <h3 class="project__description--title onTouch__description--title">
+//                     Netflix Clone
+//                 </h3>
+//                 <h4 class="project__description--sub-title onTouch__description--sub-title">
+//                     TypeScript, JavaScript, and Firebase
+//                 </h4>
+//                 <p class="project__description--para onTouch__description--para">
+//                     I created a Netflix clone using React and Firebase to practice building a dynamic and responsive web application. The app fetches data from the TMDb API to display movies and TV shows by category, supports user authentication, and is fully responsive. Through this project, I strengthened my skills in API intergation, React hooks, and UI design.
+//                 </p>
+//                 <div class="project__description--links onTouch__description--links">
+//                     <a href="https://github.com/Chaparro-ben/netflix-clone" class="project__description--link onTouch__description--link" target="_blank">
+//                         <i class="fab fa-github"></i>
+//                     </a>
+//                     <a href="https://netflix-clone-murex-gamma.vercel.app/" target="_blank" class="project__description--link onTouch__description--link">
+//                         <i class="fas fa-link"></i>
+//                     </a>
+//                 </div>
+//             </div>` 
+//     } else {
+//         displayProject.innerHTML = ""
+//     }
+// }
+// document.querySelector(".netflix__project").addEventListener("touchstart", displayNetflixProjectDescription)
